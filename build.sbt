d2ac9c6d3965fe14ac92fb3a3a18f9fd2fb6bfc3
Tony Lawrence <code@tonylawrence.com>
2016-05-12 22:28:25 +0100

    test mapping the option

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 name := "The Red Book"
 
 version := "0.0.1"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
---
+++ src/test/scala/evil/ducks/four/OptionTest.scala
@@ -0,0 +1,11 @@
+package evil.ducks.four
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class OptionTest extends FlatSpec with Matchers {
+
+  "An Option" should "map a value" in {
+    None() map identity should be(None())
+    Some(1) map(_ + 1) should be(Some(2))
+  }
+}
